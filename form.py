5fe38dd901699458cd366891f7972772b4df9302
Panas18 <panas.tiwary18@gmail.com>
2021-05-01 17:21:05 +0545

    Site form

---
+++ form.py
@@ -0,0 +1,23 @@
+from wtforms import Form, StringField, PasswordField, SubmitField, validators
+
+
+class RegistrationForm(Form):
+    username = StringField("Username",
+                           [validators.DataRequired(),
+                            validators.Length(min=2, max=20)])
+    email = StringField('Email',
+                        [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password',
+                             [validators.DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     [validators.DataRequired(),
+                                      validators.EqualTo('password')])
+    submit = SubmitField('Register')
+
+
+class LoginForm(Form):
+    email = StringField('Email',
+                        [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password',
+                             [validators.DataRequired()])
+    submit = SubmitField('Register')
