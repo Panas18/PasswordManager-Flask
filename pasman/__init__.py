9716f046ed1f2edd29010d760010e5708b47ef1d
Panas18 <panas.tiwary18@gmail.com>
2021-05-08 20:29:30 +0545

    Flash Message

--- pasman/__init__.py
+++ pasman/__init__.py
@@ -1,11 +1,17 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 db = SQLAlchemy(app)
+
 from pasman import routes
